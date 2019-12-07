812d95b93002e035e6daf1922e4b9e2106faa478
John McKee <jmckee3@mac.com>
2019-12-06 19:22:52 -0800

    wip

--- spec-special-string-agan_test.go
+++ spec-special-string-agan_test.go
@@ -2,6 +2,75 @@ package main
 
 import "testing"
 
-func TestSpecialStringAgain(t *testing.T) {
+func TestSpecialStringAgain1(t *testing.T) {
+	n := int32(8)
+	s := "mnonopoo"
 
+	possibleStrings := substrCount(n, s)
+
+	if possibleStrings != 12 {
+		t.Errorf("got %d instead of 12", possibleStrings)
+	}
 }
+
+// func TestSpecialStringAgain2(t *testing.T) {
+// 	n := int32(5)
+// 	s := "asasd"
+
+// 	possibleStrings := substrCount(n, s)
+
+// 	if possibleStrings != 12 {
+// 		t.Errorf("got %d instead of 12", possibleStrings)
+// 	}
+// }
+
+// func TestSpecialStringAgain3(t *testing.T) {
+// 	n := int32(7)
+// 	s := "abcbaba"
+
+// 	possibleStrings := substrCount(n, s)
+
+// 	if possibleStrings != 12 {
+// 		t.Errorf("got %d instead of 12", possibleStrings)
+// 	}
+// }
+
+func TestSpecialStringAgain4(t *testing.T) {
+	n := int32(4)
+	s := "aaaa"
+
+	possibleStrings := substrCount(n, s)
+
+	if possibleStrings != 10 {
+		t.Errorf("got %d instead of 10", possibleStrings)
+	}
+}
+
+func TestSpecialStringAgain5(t *testing.T) {
+	n := int32(13)
+	s := "aaaabaaacaaaa"
+
+	possibleStrings := substrCount(n, s)
+
+	if possibleStrings != -1 {
+		t.Errorf("got %d instead of -1", possibleStrings)
+	}
+}
+
+// aaaaa
+
+// aa - 4
+// aaa - 3
+// aaaa - 2
+// aaaaa - 1
+
+// aaaa
+
+// aa - 3
+// aaa - 2
+// aaaa - 1
+
+// aaa
+
+// aa - 2
+// aaa - 1
--- special-string-again.go
+++ special-string-again.go
@@ -1,3 +1,109 @@
 package main
 
+import "fmt"
+
+func substrCount(n int32, s string) int64 {
+	var combinations int64
+	var endOfFirst int32
+	var firstLength int32
+	var endOfLast int32
+	var lastLength int32
+	var start int32
+	var continueFlag = true
+
+	// so far this calculates the length of one pattern, not the combinations yet!
+	// need logic to calculate same letter repeated combinations and
+	// need logic to calculate pattern combinations
+	// need to start at the right place, (current +1) on next iteration of loop
+
+	combinations = int64(n)
+
+	for continueFlag {
+		endOfFirst = checkForRepeater(start, s, n)
+		firstLength = endOfFirst - start + 1
+		combinations += repeatCombinations(firstLength)
+		fmt.Println("endOfFirst", endOfFirst, s[start:endOfFirst+1])
+
+		if endOfFirst == n-1 {
+			fmt.Println("break due to end of string")
+			break
+		}
+
+		if s[start] == s[endOfFirst+2] {
+			fmt.Println("s[start] == s[endOfFirst+2]", s[start], s[endOfFirst+2])
+			endOfLast = checkForRepeater(endOfFirst+2, s, n)
+			lastLength = endOfLast - endOfFirst + 3
+			if firstLength < lastLength {
+				fmt.Println("adding to combinations for patterns:", firstLength)
+				combinations += int64(firstLength)
+			} else {
+				fmt.Println("adding to combinations for patterns:", lastLength)
+				combinations += int64(lastLength)
+			}
+		} else {
+			endOfLast = endOfFirst + 1
+		}
+		fmt.Println("endOfLast", endOfLast, s[start:endOfLast+1])
+
+		start = endOfFirst + 1
+		fmt.Println("start = ", endOfFirst+1)
+		fmt.Println("")
+		if start >= n-2 {
+			continueFlag = false
+		}
+	}
+
+	return combinations
+}
+
+// aaaabaaacaaaa
+// aaaabaaa
+func checkForRepeater(start int32, s string, sLength int32) int32 {
+	fmt.Println("checkForRepeater called")
+	fmt.Println(start, s, sLength)
+
+	for i := int32(start); i < sLength; i++ {
+		fmt.Println("for loop:", s[i], i)
+		if s[start] != s[i] {
+			return i - 1
+		}
+	}
+	fmt.Println("out of for loop")
+
+	return sLength - 1
+}
+
+func repeatCombinations(repeatLength int32) int64 {
+	var sum int64
+
+	for i := int64(1); i <= int64(repeatLength-1); i++ {
+		sum += i
+	}
+
+	fmt.Println("repeat combinations:", sum)
+
+	return sum
+}
+
+func checkPatternsStartingAt(start int32, s string, sLength int32) int64 {
+	var firstLength int32
+	// var middle int32
+	var lastLength int32
+	// var change int32
+
+	for i := int32(start); i < sLength; i++ {
+		firstLength = checkForRepeater(start, s, sLength)
+		startOfLast := start + firstLength + 2
+		if s[startOfLast] == s[start] {
+			lastLength = checkForRepeater(startOfLast, s, sLength)
+		}
+		if lastLength >= firstLength {
+			return 1
+		}
+
+	}
+
+	return 0
+}
+
 func main() {}
